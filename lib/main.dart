194a08122da381ae2d1ed549fe82194122cdd0d5
sahil.khan@octaware.com <sahil.khan@octaware.com>
2022-06-13 15:30:25 +0530

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:ShoppingApp/pages/HomePage.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(Main());
 }
 
-class Main extends StatelessWidget{
+class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    // TODO: implement build first build
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
